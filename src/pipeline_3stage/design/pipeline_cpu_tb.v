3f1e0cb9abb37c6b2807fc9297069fcb38d9c080
ourfool <junichirokadomoto@gmail.com>
2019-06-18 15:14:02 +0900

    Add GPIO test

--- src/pipeline_3stage/design/pipeline_cpu_tb.v
+++ src/pipeline_3stage/design/pipeline_cpu_tb.v
@@ -7,20 +7,30 @@ module pipeline_cpu_tb;
 
     reg clk;
     reg rst;
-    
+
+    wire [3:0] gpio_data_out;
     wire uart_tx;
+    
+    wire [15:0] imem_addr;
+    wire [31:0] imem_rd_data;
 
     parameter CYCLE = 10;
 
     always #(CYCLE/2) clk = ~clk;
 
-    cpu_top cpu_top(
+    cpu_top cpu_top (
         .clk(clk),
         .rst(rst),
-        .imem_rd_data_in(imem_rd_data_in),
-        .imem_addr_out(imem_rd_data_in),
+        .imem_rd_data_in(imem_rd_data),
+        .imem_addr_out(imem_addr),
         .gpio_data_out(gpio_data_out),
         .uart_tx(uart_tx)
+    );
+
+    imem imem (
+        .clk(clk),
+        .addr(imem_addr),
+        .rd_data(imem_rd_data)
     );
 
     initial begin
