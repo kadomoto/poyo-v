439fdb8c6e90690d7843f1cd4e67293e8807c718
ourfool <junichirokadomoto@gmail.com>
2019-09-25 00:43:45 +0900

    Update poyoio.c

--- software/software_tools/include/poyoio.c
+++ software/software_tools/include/poyoio.c
@@ -6,37 +6,13 @@ void digital_write(int pin, int vol) {
     volatile char* output_addr = GPO_WRADDR;
     volatile int* input_addr = GPO_RDADDR;
 
-    switch (pin) {
-        case 0:
-            if (vol == 1) {
-                *output_addr = (*input_addr | 0b0001);
-            } else if (vol == 0) {
-                *output_addr = (*input_addr & 0b1110);
-            }
-            break;
-        case 1:
-            if (vol == 1) {
-                *output_addr = (*input_addr | 0b0010);
-            } else if (vol == 0) {
-                *output_addr = (*input_addr & 0b1101);
-            }
-            break;
-        case 2:
-            if (vol == 1) {
-                *output_addr = (*input_addr | 0b0100);
-            } else if (vol == 0) {
-                *output_addr = (*input_addr & 0b1011);
-            }
-            break;
-        case 3:
-            if (vol == 1) {
-                *output_addr = (*input_addr | 0b1000);
-            } else if (vol == 0) {
-                *output_addr = (*input_addr & 0b0111);
-            }
-            break;
+    // 0ビット目は0ピンの状態、1ビット目は1ピンの状態というように値を格納しているので、
+    // ピンの値に応じたビットのみを変更する
+    if (vol == 1) {
+        *output_addr = (*input_addr | (1 << pin));
+    } else if (vol == 0) {
+        *output_addr = (*input_addr & ~(1 << pin));
     }
-
 }
 
 
@@ -45,31 +21,19 @@ int digital_read(int pin) {
     volatile int* input_addr = GPI_ADDR;
     int vol;
 
-    switch (pin) {
-        case 0:
-            vol = (*input_addr & 0b0001);
-            break;
-        case 1:
-            vol = (*input_addr & 0b0010);
-            break;
-        case 2:
-            vol = (*input_addr & 0b0100);
-            break;
-        case 3:
-            vol = (*input_addr & 0b1000);
-            break;
-    }
+    // 0ビット目は0ピンの状態、1ビット目は1ピンの状態というように値を格納しているので、
+    // ピンの値に応じて特定ビットを読み出す
+    vol = (*input_addr >> pin) & 1;
 
     return vol;
-
 }
 
 
 void serial_write(char c) {
-    
-    delay(UART_TX_DELAY_TIME);
 
     volatile char* output_addr = UART_TX_ADDR;
+
+    delay(UART_TX_DELAY_TIME);
     *output_addr = c;
 }
 
@@ -88,7 +52,6 @@ char serial_read() {
 void delay(unsigned int time) {
     
     volatile unsigned int* input_addr = HARDWARE_COUNTER_ADDR;
-
     unsigned int start_cycle = *input_addr;
 
     while (time > 0) {
@@ -100,5 +63,4 @@ void delay(unsigned int time) {
             start_cycle = *input_addr;
         }
     }
-
 }
