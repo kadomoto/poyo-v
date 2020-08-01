e3dca46795d207971aeb18d027832b3bbc1d30c5
ourfool <junichirokadomoto@gmail.com>
2020-08-01 19:21:32 +0900

    Update bootrom to change memory map

--- src/pipeline_3stage/design/bootrom.v
+++ src/pipeline_3stage/design/bootrom.v
@@ -11,13 +11,13 @@ module bootrom (
     output wire [31:0] rd_data
 );
 
-    reg [31:0] mem [0:16383];  // 64KiB(16bitアドレス空間)
-    reg [13:0] addr_sync;  // 64KiBを表現するための14bitアドレス(下位2bitはここでは考慮しない)
+    reg [31:0] mem [0:8191];  // 32KiB(8bitアドレス空間)
+    reg [12:0] addr_sync;  // 32KiBを表現するための13bitアドレス(下位2bitはここでは考慮しない)
 
     initial $readmemh({`BOOTROM_DATA_PATH, "boot.hex"}, mem);
      
     always @(posedge clk) begin
-        addr_sync <= addr[15:2];  // 読み出しアドレス更新をクロックと同期することでBRAM化
+        addr_sync <= addr[14:2];  // 読み出しアドレス更新をクロックと同期することでBRAM化
     end
     
     assign rd_data = mem[addr_sync];
