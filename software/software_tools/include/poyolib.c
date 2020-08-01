b02f60388bcb1552d90d521405171923f95e1654
ourfool <junichirokadomoto@gmail.com>
2020-08-01 22:44:37 +0900

    Add poyolib

---
+++ software/software_tools/include/poyolib.c
@@ -0,0 +1,116 @@
+#include <poyoio.h>
+#include <poyolib.h>
+
+
+void *memset(void *b, int c, long len) {
+  char *p;
+  for (p = b; len > 0; len--) {
+    *(p++) = c;
+  }
+  return b;
+}
+
+
+void *memcpy(void *dst, const void *src, long len) {
+  char *d = dst;
+  const char *s = src;
+  for (; len > 0; len--) {
+    *(d++) = *(s++);
+  }
+  return dst;
+}
+
+
+int memcmp(const void *b1, const void *b2, long len) {
+  const char *p1 = b1, *p2 = b2;
+  for (; len > 0; len--) {
+    if (*p1 != *p2)
+      return (*p1 > *p2) ? 1 : -1;
+    p1++;
+    p2++;
+  }
+  return 0;
+}
+
+
+int strlen(const char *s) {
+  int len;
+  for (len = 0; *s; s++, len++) {
+    ;
+  }
+  return len;
+}
+
+
+char *strcpy(char *dst, const char *src) {  
+  char *d = dst;
+  for (;; dst++, src++) {
+    *dst = *src;
+    if (!*src) break;
+  }
+  return d;
+}
+
+
+int strcmp(const char *s1, const char *s2) {
+  while (*s1 || *s2) {
+    if (*s1 != *s2)
+      return (*s1 > *s2) ? 1 : -1;
+    s1++;
+    s2++;
+  }
+  return 0;
+}
+
+
+int strncmp(const char *s1, const char *s2, int len) {
+  while ((*s1 || *s2) && (len > 0)) {
+    if (*s1 != *s2)
+      return (*s1 > *s2) ? 1 : -1;
+    s1++;
+    s2++;
+    len--;
+  }
+  return 0;
+}
+
+
+int putc(unsigned char c) {
+  if (c == '\n') {
+    serial_write('\r');
+  } else {
+    serial_write(c);
+  }
+  return 0;
+}
+
+
+int puts(unsigned char *str) {
+  while (*str) {
+    putc(*(str++));
+  }
+  return 0;
+}
+
+
+int putxval(unsigned long value, int column) {
+  char buf[9];
+  char *p;
+
+  p = buf + sizeof(buf) - 1;
+  *(p--) = '\0';
+
+  if (!value && !column) {
+    column++;
+  }
+
+  while (value || column) {
+    *(p--) = "0123456789abcdef"[value & 0xf];
+    value >>= 4;
+    if (column) column--;
+  }
+
+  puts(p + 1);
+
+  return 0;
+}
---
+++ software/software_tools/include/poyolib.h
@@ -0,0 +1,18 @@
+#ifndef POYOLIB_H
+#define POYOLIB_H
+
+// poyolib.c
+void *memset(void *b, int c, long len);
+void *memcpy(void *dst, const void *src, long len);
+int memcmp(const void *b1, const void *b2, long len);
+int strlen(const char *s);
+char *strcpy(char *dst, const char *src);
+int strcmp(const char *s1, const char *s2);
+int strncmp(const char *s1, const char *s2, int len);
+
+int putc(unsigned char c);
+int puts(unsigned char *str);
+int putxval(unsigned long value, int column);
+
+
+#endif
