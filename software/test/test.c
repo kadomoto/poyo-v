ca03a024f247329ed6db26bf0ce05a25bca39be5
ourfool <junichirokadomoto@gmail.com>
2020-08-02 21:49:30 +0900

    Update test

--- software/test/test.c
+++ software/test/test.c
@@ -4,30 +4,28 @@
 
 int main() {
 
-    while(1){
+    serial_write('H');
+    serial_write('E');
+    serial_write('L');
+    serial_write('L');
+    serial_write('O');
 
-        serial_write('H');
-        serial_write('E');
-        serial_write('L');
-        serial_write('L');
+    for (int i=0; i < 2; i++) {
+        serial_write('P');
         serial_write('O');
+        serial_write('Y');
+        serial_write('O');
+    }
 
-        for (int i=0; i < 2; i++) {
-            serial_write('P');
-            serial_write('O');
-            serial_write('Y');
-            serial_write('O');		
-        }
+    puts("\n\n")
 
-        delay(3000);
-
-        puts("Hello\n");
-
+    while(1){
+        puts("Press any key\n");
         char c;
         c = serial_read();
         putc(c);
+        puts("\n\n")
     }
 
     return 0;
-
 }
