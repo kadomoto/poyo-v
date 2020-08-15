d0252a4e036df6fc27e207d1977f06ef1051c8e8
ourfool <junichirokadomoto@gmail.com>
2020-08-16 02:49:36 +0900

    Update bootloader

--- software/boot/boot.c
+++ software/boot/boot.c
@@ -4,33 +4,9 @@
 
 
 static int init(void) {
-    //extern int erodata, data_start, edata, bss_start, ebss;
     extern int _rodata_end, _data_start, _data_end;
 
-    //memcpy(&data_start, &erodata, (long)&edata - (long)&data_start);
     memcpy(&_data_start, &_rodata_end, (long)&_data_end - (long)&_data_start);
-
-    return 0;
-}
-
-
-static int dump(char *buf, long size) {
-    long i;
-
-    if (size < 0) {
-        puts("no data.\n");
-        return -1;
-    }
-    for (i = 0; i < size; i++) {
-        putxval(buf[i], 2);
-        if ((i & 0xf) == 15) {
-            puts("\n");
-        } else {
-            if ((i & 0xf) == 7) puts(" ");
-            puts(" ");
-        }
-    }
-    puts("\n");
 
     return 0;
 }
@@ -57,23 +33,18 @@ int main() {
             } else {
 	            puts("\nXMODEM transfer complete\n");
             }
-        // } else if (!strcmp(buf, "dump")) {
-        //     puts("size: ");
-        //     putxval(size, 0);
-        //     puts("\n");
-        //     dump(loadbuf, size);
         } else if (!strcmp(buf, "run")) {
             puts("start\n");
-            f = (void (*)(void))entry_point;
+            f = (void (*)(void))(char *)(&_rom_start);
             f();
         } else if (!strcmp(buf, "screenfetch")) {
-            puts(" ######    #####   ##  ##    #####\n");
-            puts("  ##  ##  ##   ##  ##  ##   ##   ##\n");
-            puts("  ##  ##  ##   ##  ##  ##   ##   ##\n");
-            puts("  #####   ##   ##   ####    ##   ##\n");
-            puts("  ##      ##   ##    ##     ##   ##\n");
-            puts("  ##      ##   ##    ##     ##   ##\n");
-            puts(" ####      #####    ####     #####\n");
+            puts(" ##   ##  ######   ####\n");
+            puts(" ### ###  # ## #    ##\n");
+            puts(" #######    ##      ##\n");
+            puts(" #######    ##      ##\n");
+            puts(" ## # ##    ##      ##   #\n");
+            puts(" ##   ##    ##      ##  ##\n");
+            puts(" ##   ##   ####    #######\n");
         } else {
             puts("unknown.\n");
         }
